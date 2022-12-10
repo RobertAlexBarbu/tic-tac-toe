6959c8a1446dd9e085418788f67a1aac01811bcb
Robert Alex Barbu <robertalexbarbu@gmail.com>
2022-12-10 23:24:44 +0200

    Add close button to options

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const options = document.querySelector(".options");
 const optionsBtns = document.querySelector(".optionsBtns");
 const playBtn = document.querySelector("#playBtn");
+const closeOptionsBtn = document.querySelector("#closeOptionsBtn");
 playBtn.addEventListener('click', activateOptions);
 function activateOptions() {
     options.classList.add("active");
     optionsBtns.classList.add("active");
 }
 options.addEventListener('click', exitOptions);
+closeOptionsBtn.addEventListener('click', exitOptions);
 function exitOptions() {
     options.classList.remove("active");
     optionsBtns.classList.remove("active");

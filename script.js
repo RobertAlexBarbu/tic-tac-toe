c96a6bf45f9211e036cb35a34812a82f19c1e02d
Robert Alex Barbu <robertalexbarbu@gmail.com>
2022-12-12 03:00:02 +0200

    Refactor code
    Created a better implementation of the player vs player mode.

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
 
 const menu = (() => {
     const playBtn = document.querySelector("#playBtn");
-    const options = document.querySelector(".options");
-    const optionsBtns = document.querySelector(".optionsBtns");
-    const closeOptionsBtn = document.querySelector("#closeOptionsBtn");
-    const playerBtn = document.querySelector("#playerBtn")
-    const activateOptions = ()=> {
-        options.classList.add("active");
-        optionsBtns.classList.add("active");
-    }
-    const exitOptions = ()=> {
-        options.classList.remove("active");
-        optionsBtns.classList.remove("active");
-    }
     const create = ()=> {
-        playBtn.addEventListener('click', activateOptions);
-        options.addEventListener('click', exitOptions);
-        closeOptionsBtn.addEventListener('click', exitOptions);
-        optionsBtns.addEventListener('click', (event)=>{event.stopPropagation();});
-        playerBtn.addEventListener('click', playerGame);
+        playBtn.addEventListener('click', startGame);
     }
-    const playerGame = ()=> {
-        gameboard.startPlayerGame();
-        menu.exitOptions();
+    const startGame = ()=> {
+        playBtn.textContent = "Play";
+        gameboard.start();
     }
-    return {create, exitOptions, activateOptions};
+    return {create, playBtn};
 })();
-const playerFactory = (xo)=> {
-    return {xo, }
-}
 const gameboard = (()=> {
     let boardArray = ["A", "B", "C", "D", "E", "F", "", "", ""];
     const boardXOCells = document.querySelectorAll(".xo");
@@ -40,6 +21,11 @@ const gameboard = (()=> {
             if(boardArray[i] != "") {
                 element.parentNode.classList.add("occupied");
             }
+            else {
+                element.parentNode.classList.remove("occupied");
+                element.classList.remove("won");
+                element.classList.remove("draw");
+            }
             i++;
         });
     }
@@ -47,75 +33,69 @@ const gameboard = (()=> {
         boardArray = ["", "", "", "", "", "", "", "", ""];
         render();
     }
-    const placeEvent = (index)=> {
-        if(boardArray[index] === "") {
-            console.log(round);
-            if(round%2 == 1) {
-                boardArray[index] = "X";
-            }
-            else {
-                boardArray[index] = "O";
-            }
-            round++;
-            console.log(checkWin());
-            render();
+    const checkWinAux = (i1, i2, i3)=> {
+        if(boardArray[i1] === boardArray[i2] && boardArray[i1] === boardArray[i3] && boardArray[i1] !== "") {
+            boardXOCells[i1].classList.add("won");
+            boardXOCells[i2].classList.add("won");
+            boardXOCells[i3].classList.add("won");
+            return true;
+        }
+        else {
+            return false;
         }
     }
     const checkWin = ()=> {
         switch(true) {
-            case boardArray[0] === boardArray[1] && boardArray[0] === boardArray[2] && boardArray[0] !== "":
-            case boardArray[0] === boardArray[3] && boardArray[0] === boardArray[6] && boardArray[0] !== "":
-            case boardArray[0] === boardArray[4] && boardArray[0] === boardArray[8] && boardArray[0] !== "":
-                //return boardArray[0] + " WON";
-                menu.activateOptions();
-                break;
-            case boardArray[2] === boardArray[5] && boardArray[2] === boardArray[8] && boardArray[2] != "":
-            case boardArray[2] === boardArray[4] && boardArray[2] === boardArray[6] && boardArray[2] != "":
-                //return boardArray[2] + " WON";
-                break;
-            case boardArray[3] === boardArray[4] && boardArray[3] === boardArray[5] && boardArray[3] != "":
-                //return boardArray[3] + " WON";
-                break;
-            case boardArray[6] === boardArray[7] && boardArray[6] === boardArray[8] && boardArray[6] != "":
-                //return boardArray[6] + " WON";
-                break;
-            case boardArray[1] === boardArray[4] && boardArray[1] === boardArray[7] && boardArray[1] != "":
-                //return boardArray[1] + " WON";
-                break;
-            case round === 10:
-                return "DRAW";
+            case checkWinAux(0, 1, 2):
+            case checkWinAux(3, 4, 5):
+            case checkWinAux(6, 7, 8):
+            case checkWinAux(0, 3, 6):
+            case checkWinAux(1, 4, 7):
+            case checkWinAux(2, 5, 8):
+            case checkWinAux(0, 4, 8):
+            case checkWinAux(2, 4, 6):
+                return true;
                 break;
             default:
                 return false;
         }
+    }
 
+    function placeEvent(event) {
+        const aux = event.target.dataset.position;
+        if (boardArray[aux] === "") {
+            if(round%2===1) {
+                boardArray[aux] = "X";
+            }
+            else {
+                boardArray[aux] = "O";
+            }
+            render();
+            if(checkWin()===true) {
+                console.log(boardArray[aux]);
+                boardCells.forEach((element)=> {
+                    element.removeEventListener("click", placeEvent, true);
+                });
+                menu.playBtn.textContent = "Play Again";
+            }
+            else if(round===9) {
+                menu.playBtn.textContent = "Play Again";
+                boardXOCells.forEach((element)=> {
+                    element.classList.add("draw");
+                    
+                })
+            }
+            round++;
+        }
     }
     let round;
-    const startPlayerGame = ()=> {
+    const start = ()=> {
         clear();
         round = 1;
-        boardCells.forEach((element, index)=> {
-            const placeEvent = ()=> {
-                if(boardArray[index] === "") {
-                    console.log(round);
-                    if(round%2 == 1) {
-                        boardArray[index] = "X";
-                    }
-                    else {
-                        boardArray[index] = "O";
-                    }
-                    round++;
-                    console.log(checkWin());
-                    render();
-                }
-            }
-            element.addEventListener('click', placeEvent);
-        });        
+        boardCells.forEach((element)=> {
+            element.addEventListener("click", placeEvent, true);
+        });  
     }
-    return {render, startPlayerGame};
+    return {start, clear};
 })();
-
-
-
-
 menu.create();
